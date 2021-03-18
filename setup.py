b70479966735fcba7b267253e953b7bc4c56711a
stuart <stuart@email>
2021-03-18 15:36:36 +0000

    Add correct readme, update setup.py to reflect these changes.

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
 
 setup(
     name='bowling',
     version='0.1.0',
     description='Bowling Kata in Python',
     long_description=readme,
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    url='https://github.com/stuie382/bowling-kata-python.git',
+    packages=find_packages(exclude='tests')
 )
